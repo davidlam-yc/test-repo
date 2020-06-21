a9e7837fedf4e5366af8461b284dd44e74fb6be4
David Lam <davidlam.yc@gmail.com>
2020-06-21 17:16:22 +0800

    adsf

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# install Amazon CLI
-apt-get update && apt-get install -y python-dev
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-pip install awscli
-aws s3 rm s3://lerna-stencil/branch-del --recursive
+
+XXX=${github.head_ref}
+echo $XXX
+
---
+++ temp.sh
@@ -0,0 +1,2 @@
+GITHUB_REF=refs/heads/feature-branch-1
+echo ${GITHUB_REF##*/}
