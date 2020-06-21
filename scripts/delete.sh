8cb5c0646174c11371f365b5e5227e3da5ed3f2d
David Lam <davidlam.yc@gmail.com>
2020-06-21 17:42:16 +0800

    sdfg

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-echo "HOHOHOHOHO"
-echo $myvar
-
+# install Amazon CLI
+apt-get update && apt-get install -y python-dev
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install awscli
+IFS='/'
+read -a strarr <<< "$branch_name"
+echo "Deleting folder ${strarr[1]}"
+aws s3 rm s3://lerna-stencil/${strarr[1]} --recursive
